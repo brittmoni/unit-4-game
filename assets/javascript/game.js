ba36233d143c9faefb8e99329f1a3a7addc4d154
Brittany F <brittfranklin92@gmail.com>
2019-02-12 21:01:41 -0500

    Pseudo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+/*
+1. Create a variable that will hold the random number
+2. Create a variable that will hold the user's input
+3. Create the 4 buttons
+4. Add the crystal image to the buttons
+5. Create onclick function that will calculate a random number and display the random number
+6. Assign the random number to the user input variable
+7. Create a function that will add the user inputs and assign it to the variable
+8. if(addition of user input === computer generated random number) {
+  player wins
+}
+else {
+  player loses
+}
+9. reset game
+10. new random number generated
+*/
